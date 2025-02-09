d7749f1ee67628a63b03be0095f05be2757e7e41
hanna-uler <hannauler@gmail.com>
2025-02-09 11:38:57 -0500

    complete task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,38 @@
-function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price + deliveryFee;
-    return `Shipping to ${country} will cost ${totalPrice} credits`
-};
-console.log(getShippingMessage("Australia", 120, 50));
-console.log(getShippingMessage("Germany", 80, 20));
-console.log(getShippingMessage("Sweden", 100, 20));
+class Storage{
+    #items = [];
+    constructor(items) {
+        this.#items = items;
+     };
+    getItems() {
+        return this.#items;
+    }
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+    removeItem(itemToRemove) {
+        this.#items = this.#items.filter(item => item !== itemToRemove);
+    }
+}
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+// Створи клас Storage, який створюватиме об'єкти для управління складом товарів. 
+// Клас очікує лише один аргумент — початковий масив товарів, який записується до створеного 
+// об'єкта в приватну властивість items.
+
+// Оголоси наступні методи класу:
+
+// getItems() — повертає масив поточних товарів у приватній властивості items.
+//     addItem(newItem) — приймає новий товар newItem і додає його до масиву товарів 
+// у приватну властивість items об'єкта.
+// removeItem(itemToRemove) — приймає рядок з назвою товару itemToRemove і видаляє 
+// його з масиву товарів у приватній властивості items об'єкта.
